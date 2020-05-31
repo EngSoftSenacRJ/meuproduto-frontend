02f2ba1f1eef82ff2d74ed45974753c487703534
Romulo Rocha <rrocha@hpe.com>
2020-05-31 20:00:40 -0300

    fix nas variaveis do environment para host e porta

--- src/app/helpers/appconstants.ts
+++ src/app/helpers/appconstants.ts
@@ -1,6 +1,10 @@
 import { environment } from 'src/environments/environment';
 export class Appconstants {
     public static get baseAPIURL(): string {
+        if (environment.apiBaseHost === 'API_BASE_PORT' || environment.apiBasePort === 'API_BASE_HOST') {
+            return 'http://localhost:8080/api/';
+        }
         return 'http://'.concat(environment.apiBaseHost).concat(':').concat(environment.apiBasePort).concat('/api/');
     }
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiBaseHost: 'localhost',
-  apiBasePort: '8080'
+  apiBaseHost: 'API_BASE_HOST',
+  apiBasePort: 'API_BASE_PORT'
 };
 
 /*
