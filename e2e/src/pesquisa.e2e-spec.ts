18b0d5c3bb6d2bbd68d5685cc3a4fee07586e531
Carmen <carmenlivia.lopes@gmail.com>
2020-06-04 00:55:32 -0300

    Testes e2e realizados: - Informaçoes do produto pesquisado -

--- e2e/src/pesquisa.e2e-spec.ts
+++ e2e/src/pesquisa.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { ComponentInterface } from "./pageobject/common.po";
 import { element, by, browser } from 'protractor';
 import { protractor } from 'protractor/built/ptor';
+import { PesquisaPage } from './pageobject/pesquisa.po';
 
 var origFn = browser.driver.controlFlow().execute;
 browser.driver.controlFlow().execute = function() {
@@ -16,9 +17,11 @@ browser.driver.controlFlow().execute = function() {
  
 describe('Testando módulo de pesquisa: ',()=>{
     let elemento : ComponentInterface;
+    let pesqPage : PesquisaPage;
 
     beforeEach(()=>{
         elemento = new ComponentInterface();
+        pesqPage = new PesquisaPage();
     });
 
     it('Dever ir para a página de pesquisa:',()=>{
@@ -31,7 +34,20 @@ describe('Testando módulo de pesquisa: ',()=>{
         element(by.id('marca')).all(by.tagName('option')).get(0).click();
         element(by.id('Distancia')).all(by.tagName('option')).get(0).click();
         element(by.cssContainingText('.btn-block', 'Pesquisar')).click();        
-        browser.sleep(3000);
+      
         // browser.actions().click().perform();
     });
+
+    it('Deve abrir os detallhes do produto buscado', ()=>{
+       expect(element.all(by.repeater("pesquisa-list")).get(0).element(by.id('produto')).click()).toBeDefined();
+        browser.sleep(3000);
+        browser.actions().click().perform();
+
+    });
+
+    it('Deve abrir os detalhes da loja do produto buscado', ()=>{
+        expect(element.all(by.repeater("pesquisa-list")).get(0).element(by.id('loja')).click()).toBeDefined();
+        browser.sleep(3000);
+
+    })
 });
