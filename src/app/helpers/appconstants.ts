1c7a8e4f7eb2c286ba2a257e0b1266e60786a98c
Romulo Rocha <rrocha@hpe.com>
2020-07-17 14:11:55 -0300

    Alteraçao para IP Elastic hardcoded

--- src/app/helpers/appconstants.ts
+++ src/app/helpers/appconstants.ts
@@ -1,25 +1,25 @@
 import { environment } from 'src/environments/environment';
 export class Appconstants {
     public static get baseAPIURL(): string {
-        
+        /*
         if (environment.apiBaseHost === 'API_BASE_HOST') {
             return 'http://localhost:8080/api/';
         }
         
         return 'http://'.concat(environment.apiBaseHost).concat(':8080').concat('/api/');
-        
-       //return 'http://3.128.231.227:8080/api/';
+       */ 
+       return 'http://3.128.231.227:8080/api/';
     }
 
     public static get baseSearchAPIURL(): string {
-        
+        /*
         if (environment.apiBaseHost === 'API_BASE_HOST') {
             return 'http://localhost:8082/searchapi/';
         }
         
         return 'http://'.concat(environment.apiBaseHost).concat(':8082').concat('/searchapi/');
-        
-       //return 'http://3.128.231.227:8082/searchapi/';
+        */
+       return 'http://3.128.231.227:8082/searchapi/';
     }
 
 }
