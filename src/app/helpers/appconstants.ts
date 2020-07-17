39c07bbdda1c3342bbeabc6da11fcad577bb9761
Romulo Rocha <rrocha@hpe.com>
2020-07-16 22:30:22 -0300

    alterado para informar o IP hardcoded

--- src/app/helpers/appconstants.ts
+++ src/app/helpers/appconstants.ts
@@ -1,17 +1,25 @@
 import { environment } from 'src/environments/environment';
 export class Appconstants {
     public static get baseAPIURL(): string {
+        /*
         if (environment.apiBaseHost === 'API_BASE_PORT' || environment.apiBasePort === 'API_BASE_HOST') {
             return 'http://localhost:8080/api/';
         }
+        
         return 'http://'.concat(environment.apiBaseHost).concat(':8080').concat('/api/');
+        */
+       return 'http://18.222.83.180:8089/api/';
     }
 
     public static get baseSearchAPIURL(): string {
+        /*
         if (environment.apiBaseHost === 'API_BASE_PORT' || environment.apiBasePort === 'API_BASE_HOST') {
             return 'http://localhost:8082/searchapi/';
         }
+        
         return 'http://'.concat(environment.apiBaseHost).concat(':8082').concat('/searchapi/');
+        */
+       return 'http://18.222.83.180:8082/searchapi/';
     }
 
 }
