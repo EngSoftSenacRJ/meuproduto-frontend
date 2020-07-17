bf7d4155a3ac81b7ab848aea1303e8f81b41d8a7
Romulo Rocha <rrocha@hpe.com>
2020-07-17 20:00:41 -0300

    ajustes nas marcas e produto

--- src/app/helpers/appconstants.ts
+++ src/app/helpers/appconstants.ts
@@ -16,14 +16,10 @@ export class Appconstants {
         if (environment.apiBaseHost === 'API_BASE_HOST') {
             return 'http://localhost:8082/searchapi/';
         }
-<<<<<<< HEAD
-        return 'http://'.concat(environment.apiBaseHost).concat(':').concat(environment.apiBasePort).concat('/searchapi/');
-=======
         
         return 'http://'.concat(environment.apiBaseHost).concat(':8082').concat('/searchapi/');
         */
        return 'http://18.218.14.149:8082/searchapi/';
->>>>>>> da586e77f35dc7d9981aa0bf1bf0397a02c94ad0
     }
 
 }
